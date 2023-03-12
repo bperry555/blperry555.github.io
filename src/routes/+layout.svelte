3cbf4e55f8dc06dff2fb6d6c75321595f9256bd0
Brian <blperry5@gmail.com>
2023-03-12 03:17:09 -0700

    First commit, Setup and Config for Sveltekit static site for GitHub Pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import './globals.scss';
+	import MainNav from '$lib/MainNav.svelte';
+</script>
+
+<svelte:head>
+	<title>Brian Perry | Data Analytics | Software Engineering | Automation | Web Development</title>
+</svelte:head>
+
+<MainNav />
+
+<slot />
+
+<style lang="scss">
+	@use "../../static/styles/vars.scss";
+	body {
+		background-color: vars.$bg-dark;
+		color: vars.$primary-color;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,53 @@
+<section>
+	<div class="box">
+		<h1>Hello, I'm Brian <span>&#x1F44B;</span></h1>
+		<p class="skills-list">
+			Problem Solver <span class="pipe">|</span> Data Analytics <span class="pipe">|</span> 
+			Data Visualizations <span class="pipe">|</span>
+		</p>
+		<p class="skills-list">	
+			 Software Engineering <span class="pipe">|</span> Automation <span class="pipe">|</span> Web Development <span class="pipe">|</span>
+		</p>
+		<p class="location">San Francisco CA.</p>
+	</div>
+</section>
+
+<style lang="scss">
+	@use "../../static/styles/vars.scss";
+
+	section {
+		display: grid;
+		grid-template-columns: 20% 60% 20%;
+		margin: 8em 10em 0 10em;
+
+		.box {
+			grid-column: 2 / 3;
+			padding: 1rem 2rem;
+			border: 3px;
+			border-radius: 20px;
+			border-style: outset;
+			border-color: vars.$primary-color;
+			box-shadow: 10px 10px 5px 2px;
+
+			h1 {
+				font-family: vars.$secondary-font;
+			}
+			p {
+				font-family: vars.$primary-font;
+				font-weight: 400;
+				font-size: 1.5rem;
+				line-height: 2rem;
+			}
+			.location {
+				font-size: 1.1rem;
+				font-style: italic;
+			}
+
+			.pipe {
+				color: purple;
+				font-weight: bold;
+			}
+			background-color: vars.$text-box-light;
+		}
+	}
+</style>
